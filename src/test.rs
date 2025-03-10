175d7e2ab929ab44c8b0ecb41aecc6cd9c31587c
rrx1c <rrx1c@proton.me>
2025-03-10 19:43:31 +0100

    Made my own string struct for strings on the stack because who need allocation when the buffer isn't supposed to grow

---
+++ src/test.rs
@@ -0,0 +1,8 @@
+//Author: RR28
+//Discord: rrx1c
+//Jabber: rrx1c@jabber.fr
+//Github profile: https://github.com/RRx1C
+//Link to repo: https://github.com/RRx1C/lunettes-mips-rs
+
+#![no_std]
+pub mod lm_mips;
